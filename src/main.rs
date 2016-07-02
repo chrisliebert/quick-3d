d21710eaf0e29023e5300daba7f5650a20c9628b
chrisliebert <liebert@pdx.edu>
2016-07-02 15:18:51 -0700

    Added diffuse textures to the example program

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 // Copyright(C) 2016 Chris Liebert
 #[macro_use]
 extern crate glium;
+extern crate image;
 extern crate nalgebra;
 extern crate quick_3d;
+
+use std::collections::HashMap;
 
 use glium::glutin;
 use glium::glutin::Event;
@@ -17,28 +20,27 @@ use quick_3d::loader;
 use nalgebra::{Matrix4, PerspectiveMatrix3};
 
 fn main() {
-    let screen_width = 800;
-    let screen_height = 600;
-
+    let screen_width = 400;
+    let screen_height = 300;
     let db_file: &str = "test.db";
-
+    
     let scene = loader::load_db(db_file);
 
     let display = glutin::WindowBuilder::new()
         //.resizable()
         //.with_vsync()
         //with_gl_debug_flag(true)
-        .with_visibility(false)
+        .with_visibility(false) // Window is shown when scene finishes loading.
         .with_dimensions(screen_width, screen_height)
         .build_glium()
         .unwrap();
 
-    let mut vertex_buffers: Vec<glium::vertex::VertexBuffer<common::Vertex8f32>> = Vec::new();
+    let mut vertex_buffers: Vec<glium::vertex::VertexBuffer<common::Vertex8f32>> = Vec::with_capacity(scene.meshes.len());
 
     for i in 0..scene.meshes.len() {
         vertex_buffers.push(glium::vertex::VertexBuffer::new(&display, &scene.meshes[i].vertices).unwrap());
     }
-
+	
     let index_buffer: glium::index::NoIndices =
         glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
@@ -47,13 +49,28 @@ fn main() {
         return;
     }
 
+	let mut textures: HashMap<String, glium::texture::CompressedSrgbTexture2d> = HashMap::new();
+
+	for i in 0..scene.images.len() {
+		//TODO: determine image format by name extension if nessisary
+		//let image = image::load_from_memory_with_format(&scene.images[i].image, image::PNG).unwrap().to_rgba();
+		let image = image::load_from_memory(&scene.images[i].image).unwrap().to_rgba();
+
+	    let image_dimensions = image.dimensions();
+	    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(),
+	                                                                   image_dimensions);
+	    let opengl_texture: glium::texture::CompressedSrgbTexture2d = glium::texture::CompressedSrgbTexture2d::new(&display, image).unwrap();
+		textures.insert(scene.images[i].name.clone(), opengl_texture);		
+	}
+
+
+	// TODO: generate this texture instead of loading from sqlite
+	let default_blank_texture = &textures["DEFAULT_BLANK_TEXTURE.png"];
     // Load the first shader in the database by default
     // TODO: load from config settings table?
     let shader_index = 0;
 
     // println!("Using glsl version {}", glsl_version);
-
-    // TODO: wrap program! macro in another macros
 
     let program: Program = program!(&display,
     	/*
@@ -143,20 +160,25 @@ fn main() {
     }
 
     while running {
-
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 0.0, 1.0);
 
         for i in 0..vertex_buffers.len() {
-            let mesh: common::Mesh = scene.meshes[i as usize].clone();
-            let material_id: i32 = mesh.material_id;
-            let material: common::Material = scene.materials[material_id as usize].clone();
-
+            let material_id: i32  = scene.meshes[i as usize].material_id.clone();
+			let diffuse = scene.materials[material_id as usize].diffuse.clone();
+			let diffuse_texname: String = scene.materials[material_id as usize].diffuse_texname.clone();
+			let opengl_texture: &glium::texture::CompressedSrgbTexture2d;
+			if diffuse_texname.len() == 0 {
+				opengl_texture = default_blank_texture;
+			} else {
+				opengl_texture = &textures[&diffuse_texname];
+			}
             let uniforms = uniform! {
 		        projection: *perspective_matrix.as_ref(),
 		        modelview: modelview_matrix_array,
 		        light_position_worldspace: [2.0, 10.0, 1.0f32],
-		        diffuse: material.diffuse,
+		        diffuse: diffuse,
+		        diffuse_texture: opengl_texture,
 		    };
 
             target.draw(&vertex_buffers[i],
@@ -179,5 +201,4 @@ fn main() {
             }
         }
     }
-
 }
