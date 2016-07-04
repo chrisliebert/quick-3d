2a2155539aabe1349d985027e17ab016a15eae14
chrisliebert <liebert@pdx.edu>
2016-07-03 18:49:28 -0700

    updated unit test so it will compile, added new load_and_render_db() unit test to demonstrate basic usage.

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,16 @@ pub mod renderer;
 #[cfg(test)]
 mod tests {
     use glium::glutin;
-
-    use glium::glutin::WindowBuilder;
     use glium::backend::glutin_backend::GlutinFacade;
     use glium::DisplayBuild;
     use loader;
-    use common::{Scene, Vertex8f32};
+    use common::Scene;
+    use renderer;
 
-    fn create_test_display(scene: &Scene) -> GlutinFacade {
+    fn create_test_display() -> GlutinFacade {
         let display = glutin::WindowBuilder::new()
             .with_visibility(false)
-            //.with_dimensions(screen_width, screen_height)
+            .with_gl_debug_flag(true)
             .build_glium()
             .unwrap();
         return display;
@@ -35,13 +34,22 @@ mod tests {
 
     #[test]
     fn loader_load_db_not_empty() {
-        println!("Running load_db test");
-
+        println!("Running loader_load_db_not_empty test");
         let scene: Scene = load_test_scene();
-        println!("loaded {} vertices", scene.vertices.len());
-        assert!(scene.vertices.len() > 0);
-        assert!(scene.scene_nodes.len() > 0);
+        assert!(scene.meshes.len() > 0);
         assert!(scene.materials.len() > 0);
         assert!(scene.shaders.len() > 0);
+    }
+
+    #[test]
+    fn load_and_render_db() {
+        println!("Running load_and_render_db test");
+        let display = create_test_display();
+        let scene: Scene = load_test_scene();
+        assert!(scene.meshes.len() > 0);
+        assert!(scene.materials.len() > 0);
+        assert!(scene.shaders.len() > 0);
+        let renderer: renderer::Renderer = renderer::Renderer::new(&display, scene);
+        renderer.render(&display);
     }
 }
