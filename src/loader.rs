1f07db2efbdd4635aa1e98891ddebf2d0939b591
chrisliebert <liebert@pdx.edu>
2016-07-04 13:11:13 -0700

    changed loader interface to have shaders queried separately from scene data.

--- src/loader.rs
+++ src/loader.rs
@@ -8,6 +8,10 @@ use std::path::Path;
 
 use common::{ImageBlob, Material, Mesh, SceneNode, Scene, Shader, Vertex8f32};
 
+pub struct DBLoader {
+    filename: String,
+}
+
 fn create_vertex8f32(px: f64,
                      py: f64,
                      pz: f64,
@@ -27,144 +31,167 @@ fn create_vertex8f32(px: f64,
     }
 }
 
-pub fn load_db(filename: &str) -> Scene {
-    println!("Loading {}", filename);
-
-    let path: &Path = Path::new(filename);
-    let conn = Connection::open(path).unwrap();
-
-    // Load vertices
-    let mut vertex_stmt = conn.prepare("SELECT px, py, pz, nx, ny, nz, tu, tv FROM vertex")
-        .unwrap();
-    let vertex_iter = vertex_stmt.query_map(&[], |row| {
-            create_vertex8f32(row.get(0),
-                              row.get(1),
-                              row.get(2),
-                              row.get(3),
-                              row.get(4),
-                              row.get(5),
-                              row.get(6),
-                              row.get(7))
-        })
-        .unwrap();
-
-    let mut vertices: Vec<Vertex8f32> = Vec::new();
-
-    for vertex in vertex_iter {
-        vertices.push(vertex.unwrap());
+impl DBLoader {
+    pub fn new(filename: &str) -> DBLoader {
+        DBLoader { filename: String::from(filename) }
     }
 
-    // Load materials
-    let mut material_stmt =
-        conn.prepare("SELECT name, diffuse_r, diffuse_g, diffuse_b, diffuse_texname FROM material")
+    pub fn load_scene(&self) -> Scene {
+        let conn = Connection::open(Path::new(&self.filename)).unwrap();
+
+        // Load vertices
+        let mut vertex_stmt = conn.prepare("SELECT px, py, pz, nx, ny, nz, tu, tv FROM vertex")
             .unwrap();
-    let material_iter = material_stmt.query_map(&[], |row| {
-            let diffuse_r: f64 = row.get(1);
-            let diffuse_g: f64 = row.get(2);
-            let diffuse_b: f64 = row.get(3);
-            Material {
-                name: row.get(0),
-                diffuse: [diffuse_r as f32, diffuse_g as f32, diffuse_b as f32],
-                diffuse_texname: row.get(4),
-            }
-        })
-        .unwrap();
-
-    let mut materials: Vec<Material> = Vec::new();
-
-    for material in material_iter {
-        materials.push(material.unwrap());
-    }
-
-    let mut meshes: Vec<Mesh> = Vec::new();
-
-    // Load scene nodes
-    let mut scene_node_stmt =
-        conn.prepare("SELECT name, material_id, start_position, end_position FROM scene_node")
+        let vertex_iter = vertex_stmt.query_map(&[], |row| {
+                create_vertex8f32(row.get(0),
+                                  row.get(1),
+                                  row.get(2),
+                                  row.get(3),
+                                  row.get(4),
+                                  row.get(5),
+                                  row.get(6),
+                                  row.get(7))
+            })
             .unwrap();
-    let scene_node_iter = scene_node_stmt.query_map(&[], |row| {
-            let material_id: i32 = row.get(1);
-            let material_index: usize = material_id as usize - 1 as usize;
-            SceneNode {
-                name: row.get(0),
-                material_index: material_index, // index starts at 1 in database, not 0
-                start_position: row.get(2),
-                end_position: row.get(3),
-            }
-        })
-        .unwrap();
 
-    for scene_node in scene_node_iter {
-        let sn = scene_node.unwrap();
-        let mut new_vertices: Vec<Vertex8f32> = Vec::new();
+        let mut vertices: Vec<Vertex8f32> = Vec::new();
 
-        for i in sn.start_position as usize..(sn.end_position) as usize {
-            new_vertices.push(vertices[i]);
-
+        for vertex in vertex_iter {
+            vertices.push(vertex.unwrap());
         }
 
-        if sn.material_index >= materials.len() {
-            panic!("Material index {} out of bounds", sn.material_index);
+        // Load materials
+        let mut material_stmt =
+            conn.prepare("SELECT name, diffuse_r, diffuse_g, diffuse_b, diffuse_texname FROM \
+                          material")
+                .unwrap();
+        let material_iter = material_stmt.query_map(&[], |row| {
+                let diffuse_r: f64 = row.get(1);
+                let diffuse_g: f64 = row.get(2);
+                let diffuse_b: f64 = row.get(3);
+                Material {
+                    name: row.get(0),
+                    diffuse: [diffuse_r as f32, diffuse_g as f32, diffuse_b as f32],
+                    diffuse_texname: row.get(4),
+                }
+            })
+            .unwrap();
+
+        let mut materials: Vec<Material> = Vec::new();
+
+        for material in material_iter {
+            materials.push(material.unwrap());
         }
 
-        let mesh = Mesh {
-            name: sn.name,
-            material_index: sn.material_index,
-            vertices: new_vertices,
+        let mut meshes: Vec<Mesh> = Vec::new();
+
+        // Load scene nodes
+        let mut scene_node_stmt =
+            conn.prepare("SELECT name, material_id, start_position, end_position FROM scene_node")
+                .unwrap();
+        let scene_node_iter = scene_node_stmt.query_map(&[], |row| {
+                let material_id: i32 = row.get(1);
+                let material_index: usize = material_id as usize - 1 as usize;
+                SceneNode {
+                    name: row.get(0),
+                    material_index: material_index, // index starts at 1 in database, not 0
+                    start_position: row.get(2),
+                    end_position: row.get(3),
+                }
+            })
+            .unwrap();
+
+        for scene_node in scene_node_iter {
+            let sn = scene_node.unwrap();
+            let mut new_vertices: Vec<Vertex8f32> = Vec::new();
+
+            for i in sn.start_position as usize..(sn.end_position) as usize {
+                new_vertices.push(vertices[i]);
+
+            }
+
+            if sn.material_index >= materials.len() {
+                panic!("Material index {} out of bounds", sn.material_index);
+            }
+
+            let mesh = Mesh {
+                name: sn.name,
+                material_index: sn.material_index,
+                vertices: new_vertices,
+            };
+            meshes.push(mesh);
+        }
+
+        println!("Loaded {} vertices in {} meshes",
+                 vertices.len(),
+                 meshes.len());
+
+        // Free up some memory
+        vertices.clear();
+
+
+        // Load textures
+        let mut texture_stmt = conn.prepare("SELECT name, image FROM texture")
+            .unwrap();
+        let texture_iter = texture_stmt.query_map(&[], |row| {
+                ImageBlob {
+                    name: row.get(0),
+                    image: row.get(1),
+                }
+            })
+            .unwrap();
+
+        let mut textures: Vec<ImageBlob> = Vec::new();
+
+        for texture in texture_iter {
+            textures.push(texture.unwrap());
+        }
+
+        // conn.close();
+        return Scene {
+            materials: materials,
+            // scene_nodes: scene_nodes,
+            // vertices: vertices,
+            meshes: meshes,
+            // shaders: shaders,
+            images: textures,
         };
-        meshes.push(mesh);
     }
 
-    println!("Loaded {} vertices in {} meshes",
-             vertices.len(),
-             meshes.len());
+    pub fn load_shader(&self, name: &str, glsl_version_string: &str) -> Shader {
+        let conn = Connection::open(Path::new(&self.filename)).unwrap();
+        let use_gles = false; // Use OpenGLES instead of OpenGL (for mobile devices)
+        let glsl_version = 110;
 
-    // Free up some memory
-    vertices.clear();
+        // Load the first shader in the database by default
+        // TODO: load from config settings table?
+        let shader_index = 0;
 
-
-    // Load textures
-    let mut texture_stmt = conn.prepare("SELECT name, image FROM texture")
-        .unwrap();
-    let texture_iter = texture_stmt.query_map(&[], |row| {
-            ImageBlob {
-                name: row.get(0),
-                image: row.get(1),
-            }
-        })
-        .unwrap();
-
-    let mut textures: Vec<ImageBlob> = Vec::new();
-
-    for texture in texture_iter {
-        textures.push(texture.unwrap());
-    }
-
-    // Load shaders
-    let mut shader_stmt =
-        conn.prepare("SELECT name, vertex_source_140, fragment_source_140 FROM shader")
+        let mut shader_id: i32 = conn.query_row("SELECT id FROM shader WHERE name = 'default'", //
+                       &[],
+                       |row| row.get(0))
             .unwrap();
-    let shader_iter = shader_stmt.query_map(&[], |row| {
-            Shader {
-                name: row.get(0),
-                vertex_source_140: row.get(1),
-                fragment_source_140: row.get(2),
-            }
-        })
-        .unwrap();
 
-    let mut shaders: Vec<Shader> = Vec::new();
+        println!("Default shader id={}", shader_id);
 
-    for shader in shader_iter {
-        shaders.push(shader.unwrap());
+        let mut vertex_source: String =
+            conn.query_row("SELECT source FROM shader_version WHERE shader_id=1 AND version = \
+                            110 AND type = 'vertex'", //
+                           &[],
+                           |row| row.get(0))
+                .unwrap();
+
+        let mut fragment_source: String =
+            conn.query_row("SELECT source FROM shader_version WHERE shader_id=1 AND version = \
+                            110 AND type = 'fragment'",
+                           &[],
+                           |row| row.get(0))
+                .unwrap();
+
+        Shader {
+            name: String::from(name),
+            vertex_source: vertex_source,
+            fragment_source: fragment_source,
+        }
     }
-
-    return Scene {
-        materials: materials,
-        // scene_nodes: scene_nodes,
-        // vertices: vertices,
-        meshes: meshes,
-        shaders: shaders,
-        images: textures,
-    };
 }
