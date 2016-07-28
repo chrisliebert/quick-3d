615741dc666e9a740786fdd3f84baced0a9563a3
Chris Liebert <liebert@pdx.edu>
2016-07-28 00:43:08 -0700

    changed C structs to void*.

--- quick3d.h
+++ quick3d.h
@@ -15,32 +15,25 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef struct DBLoader {
-	char* filename;
-} DBLoader;
-
-typedef struct Camera {
-	float *modelview_matrix[4][4];
-	float *projection_matrix[4][4];
-} Camera;
-
+typedef void* DBLoader;
+typedef void* Camera;
 typedef void* Renderer;
 typedef void* Shader;
 typedef void* Window;
 
-extern DBLoader* create_db_loader(const char* filename);
-extern Camera* create_camera(float screen_width, float screen_height);
-extern Renderer create_renderer_from_db_loader(DBLoader* loader, Window window);
+extern DBLoader create_db_loader(const char* filename);
+extern Camera create_camera(float screen_width, float screen_height);
+extern Renderer create_renderer_from_db_loader(DBLoader loader, Window window);
 extern Window create_window(int screen_width, int screen_height, const char* title);
 
-extern void free_db_loader(DBLoader* dbloader);
-extern void free_camera(Camera* camera);
+extern void free_db_loader(DBLoader dbloader);
+extern void free_camera(Camera camera);
 extern void free_renderer(Renderer renderer);
 extern void free_shader(Shader shader);
 extern void free_window(Window memory);
 
-extern Shader get_shader_from_db_loader(const char* name, DBLoader* dbloader, Renderer renderer, Window display);
+extern Shader get_shader_from_db_loader(const char* name, DBLoader dbloader, Renderer renderer, Window display);
 extern int poll_quit_event(Window window);
-extern void render(Renderer renderer, Shader shader, Camera* camera, Window window);
+extern void render(Renderer renderer, Shader shader, Camera camera, Window window);
 
 #endif
