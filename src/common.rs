aa9cab74130603fad3f8250992b1486cce719026
Chris Liebert <liebert@pdx.edu>
2016-07-31 01:32:03 -0700

    Added camera functionality adapted from sdlgl3-wavefront

--- src/common.rs
+++ src/common.rs
@@ -30,12 +30,6 @@ pub struct Vertex8f32 {
 
 use std::cell::RefCell;
 
-#[derive(Debug)]
-pub struct Camera {
-    pub modelview_matrix: RefCell<[[f32; 4]; 4]>,
-    pub projection_matrix: RefCell<[[f32; 4]; 4]>,
-}
-
 #[derive(Clone)]
 pub struct Mesh {
     pub name: String,
@@ -55,7 +49,22 @@ pub struct Scene {
     pub images: Vec<ImageBlob>,
 }
 
-use nalgebra::{Matrix4, PerspectiveMatrix3};
+use nalgebra;
+use nalgebra::{Eye, Isometry3, Matrix4, PerspectiveMatrix3, ToHomogeneous, Vector3};
+
+#[derive(Debug)]
+pub struct Camera {
+    pub modelview_matrix: RefCell<[[f32; 4]; 4]>,
+    pub projection_matrix: RefCell<[[f32; 4]; 4]>,
+    pub position: RefCell<Vector3<f32>>,
+    pub direction: RefCell<Vector3<f32>>,
+    pub right: RefCell<Vector3<f32>>,
+    pub up: RefCell<Vector3<f32>>,
+    pub horizontal_angle: RefCell<f64>,
+    pub vertical_angle: RefCell<f64>,
+}
+
+use std::f64::consts::{FRAC_PI_2, PI};
 
 impl Camera {
     pub fn new(screen_width: f32, screen_height: f32) -> Camera {
@@ -64,14 +73,80 @@ impl Camera {
             PerspectiveMatrix3::new(screen_width / screen_height, 45.0, 0.1, 1000.0);
         let projection_matrix: Matrix4<f32> = projection_matrix.to_matrix();
 
-        let modelview_matrix_array = [[1.0f32, -0.0f32, -0.0f32, 0.0f32],
-                                      [0.0f32, 1.0f32, -0.0f32, 0.0f32],
-                                      [0.0f32, 0.0f32, 1.0f32, 0.0f32],
-                                      [-5.0f32, -3.0f32, -12.0f32, 1.0f32]];
+		let matrix: Matrix4<f32> = Eye::new_identity(4);
+        let modelview_matrix_array = *matrix.as_ref();
 
-        Camera {
+        let camera: Camera = Camera {
             modelview_matrix: RefCell::new(modelview_matrix_array),
             projection_matrix: RefCell::new(*projection_matrix.as_ref()),
-        }
+            position: RefCell::new(Vector3::new(0.0f32, 1.0f32, 0.0f32)),
+            direction: RefCell::new(Vector3::new(0.0f32, 0.0f32, 1.0f32)),
+            right: RefCell::new(Vector3::new(1.0f32, 0.0f32, 0.0f32)),
+            up: RefCell::new(Vector3::new(0.0f32, 1.0f32, 0.0f32)),
+            horizontal_angle: RefCell::new(PI),
+            vertical_angle: RefCell::new(0.0),
+        };
+        camera.aim(0.0, 0.0);
+        camera.update();
+        return camera;
+    }
+    
+    pub fn aim(&self, x: f64, y: f64) {
+    	let factor = 0.01;
+    	let mut horizontal = *self.horizontal_angle.borrow();
+    	let mut vertical = *self.vertical_angle.borrow();
+    	horizontal += x * factor;
+    	vertical += y * factor;
+    	
+    	let mut direction: Vector3<f32> = *self.direction.borrow();
+    	direction.x = (vertical.cos() * horizontal.sin()) as f32;
+    	direction.y = vertical.sin() as f32;
+    	direction.z = (vertical.cos() * horizontal.cos()) as f32;
+    	
+    	let mut right: Vector3<f32> = *self.right.borrow();
+    	right.x = (horizontal - FRAC_PI_2).sin() as f32;
+    	right.y = 0.0f32;
+    	right.z = (horizontal - FRAC_PI_2).cos() as f32;
+    	
+    	*self.horizontal_angle.borrow_mut() = horizontal;
+    	*self.vertical_angle.borrow_mut() = vertical;
+    	*self.direction.borrow_mut() = direction;
+    	*self.right.borrow_mut() = right;
+    	*self.up.borrow_mut() = nalgebra::cross(&right, &direction);
+    }
+    
+    pub fn move_backward(&self, amount: f32) {
+		self.move_forward(-1.0 * amount);
+    }
+    
+    pub fn move_forward(&self, amount: f32) {
+    	let direction: Vector3<f32> = *self.direction.borrow();
+    	let position: Vector3<f32> = *self.position.borrow();
+    	let scaled_direction: Vector3<f32> = amount * direction;
+    	*self.position.borrow_mut() = position + scaled_direction;
+    }
+    
+    pub fn move_left(&self, amount: f32) {
+    	self.move_right(-1.0 * amount);
+    }
+    
+    pub fn move_right(&self , amount: f32) {
+    	let right: Vector3<f32> = *self.right.borrow();
+    	let position: Vector3<f32> = *self.position.borrow();
+    	let scaled_right: Vector3<f32> = amount * right;
+    	*self.position.borrow_mut() = position + scaled_right;
+    }
+    
+    pub fn update(&self) {
+    	let position: Vector3<f32> = *self.position.borrow();
+    	let direction: Vector3<f32> = *self.direction.borrow();
+    	let up: Vector3<f32> = *self.up.borrow();
+    	
+    	let iso3 = Isometry3::look_at_rh(
+    		&position.to_point(), &(position + direction).to_point(), &up
+    	);
+    	let matrix: Matrix4<f32> = iso3.to_homogeneous();
+    	let arr: [[f32; 4]; 4] = *matrix.as_ref();
+    	*self.modelview_matrix.borrow_mut() = arr;
     }
 }
