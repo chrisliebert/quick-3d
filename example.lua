2aa7e2cbb7b7299bd4a2d641c7167a223bf5b52b
Chris Liebert <liebert@pdx.edu>
2016-08-06 14:03:23 -0700

    Corrected indentation for lua example

--- example.lua
+++ example.lua
@@ -24,61 +24,61 @@ console = quick3d.create_console_reader()
 mouse_factor = 0.01
 
 local function get_context()
-	local context = {}
+  local context = {}
 
-	-- Put all the global variables in the command context
-	setmetatable(context, { __index = _G })
-	
-	context.string = string
-	context.table = table
-	return context
+  -- Put all the global variables in the command context
+  setmetatable(context, { __index = _G })
+
+  context.string = string
+  context.table = table
+  return context
 end
-	
+
 local function execute_command(command, environment)
-    -- TODO: only use the depricated setenv()/loadstring() if load() is unavailible
-	-- See http://stackoverflow.com/questions/9268954/lua-pass-context-into-loadstring
-	
-	local f = loadstring(command)
-    if f == nil then
-      -- If f is nill, it could still be an expression in which case we return it and print the value 
-	  f = loadstring("return " .. command)
-	  assert(f)
-	  -- The enviroment must be set in order to access the scripts global variables
-	  setfenv(f, environment)
-      result = f()
-      print("Expression returned "..result)
-    else
-      -- Otherwise we just call the function
-      local result = f()
-	  if not result == nil then
-         -- If the function returns a value, print it
-         print("Function returned ".. result)
-      end
+  -- TODO: only use the depricated setenv()/loadstring() if load() is unavailible
+  -- See http://stackoverflow.com/questions/9268954/lua-pass-context-into-loadstring
+
+  local f = loadstring(command)
+  if f == nil then
+    -- If f is nill, it could still be an expression in which case we return it and print the value 
+    f = loadstring("return " .. command)
+    assert(f)
+    -- The enviroment must be set in order to access the scripts global variables
+    setfenv(f, environment)
+    result = f()
+    print("Expression returned "..result)
+  else
+    -- Otherwise we just call the function
+    local result = f()
+    if not result == nil then
+      -- If the function returns a value, print it
+      print("Function returned ".. result)
     end
+  end
 end
 
 local console_command = ""
 while not quick3d.console_is_closed(console) do
-    quick3d.render(renderer, shader, camera, display)
-    local input = quick3d.poll_event(display)
-    if input.mouse.left_button_pressed and not (input.mouse.dx == 0 and input.mouse.dy == 0) then
-        quick3d.camera_aim(camera, input.mouse.dx * mouse_factor, input.mouse.dy * mouse_factor)
-        quick3d.camera_update(camera)
+  quick3d.render(renderer, shader, camera, display)
+  local input = quick3d.poll_event(display)
+  if input.mouse.left_button_pressed and not (input.mouse.dx == 0 and input.mouse.dy == 0) then
+    quick3d.camera_aim(camera, input.mouse.dx * mouse_factor, input.mouse.dy * mouse_factor)
+    quick3d.camera_update(camera)
+  end
+  -- Read from console buffer
+  console_command = quick3d.read_console_buffer(console)
+  if string.len(console_command) > 0 then	
+    local context = get_context()
+    local success, errormsg = pcall(execute_command, console_command, context)
+    if not success then
+      print ("Failed to execute command: " .. console_command)
+      print("Error: " .. errormsg)
     end
-    -- Read from console buffer
-    console_command = quick3d.read_console_buffer(console)
-    if string.len(console_command) > 0 then	
-		local context = get_context()
-        local success, errormsg = pcall(execute_command, console_command, context)
-		if not success then
-            print ("Failed to execute command: " .. console_command)
-			print("Error: " .. errormsg)
-        end
-    end
-   
-    if input.closed then
-        quick3d.window_hide(display)
-    end
+  end
+
+  if input.closed then
+    quick3d.window_hide(display)
+  end
 end
 
 quick3d.wait_console_quit(console)
