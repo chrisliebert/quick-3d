4f300178a1d4c2617597c6042b0facc93466a11a
Chris Liebert <liebert@pdx.edu>
2016-09-15 13:26:17 -0700

    formatted indentation

--- dependencies/obj2sqlite/obj2sqlite.cpp
+++ dependencies/obj2sqlite/obj2sqlite.cpp
@@ -4,18 +4,18 @@
 
 int main(int argc, char** argv)
 {
-    if(argc < 2 || argc > 3)
-    {
-        cerr << "Usage: " << argv[0] << " objfile.obj [texture directory]" << endl;
-        return -1;
+	if(argc < 2 || argc > 3)
+	{
+		cerr << "Usage: " << argv[0] << " objfile.obj [texture directory]" << endl;
+		return -1;
 	}
-    if(strlen(argv[1]) < 5)
-    {
-        cerr << "Must end in .obj" << endl;
-        return -2;
-    }
-    string inputFileName(argv[1]);
-    string outFileName(inputFileName.substr(0, strlen(argv[1]) - 4) + ".db");
+	if(strlen(argv[1]) < 5)
+	{
+		cerr << "Must end in .obj" << endl;
+		return -2;
+	}
+	string inputFileName(argv[1]);
+	string outFileName(inputFileName.substr(0, strlen(argv[1]) - 4) + ".db");
 	wavefrontToSQLite(inputFileName.c_str(), outFileName.c_str());
-    return 0;
+	return 0;
 }
