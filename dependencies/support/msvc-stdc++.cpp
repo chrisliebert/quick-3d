4ec22f83b1c4b214bc313f03601e7a0ecc8c75db
Chris Liebert <liebert@pdx.edu>
2016-09-21 18:25:52 -0700

    deleted msvc-stdc++.cpp, it is replaced by dummy.cpp

--- dependencies/support/msvc-stdc++.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2016 Chris Liebert
-#include <Windows.h>
-
-// On MSVC, stdc++ doesn'nt need to be linked explicitly
-// This file is a placeholder that allows static libraries
-// using stdc++ to be linked in a cross-platform way.
